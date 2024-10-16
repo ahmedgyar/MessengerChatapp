33ac53f70b6f1e19f7e025ff7e54537bde2a2767
ahmedgyar <ahmedyassergyar@gmail.com>
2024-10-16 21:12:25 +0300

    chat app

--- app/components/inputs/select.tsx
+++ app/components/inputs/select.tsx
@@ -35,8 +35,7 @@ const Select: React.FC<SelectProps> = ({
           menuPortalTarget={document.body}
           isMulti
           styles={{
-            // @ts-expect-error Bug
-            menuPortal: (base) => ({
+            menuPortal: (base: any) => ({
               ...base,
               zIndex: 9999,
             }),
